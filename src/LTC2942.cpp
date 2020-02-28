2ff0cc687490a3a01374ab37e57312e071b2593a
UT2UH <oles@yaremenko.ws>
2020-02-28 09:40:45 +0200

    Update LTC2942.cpp

--- src/LTC2942.cpp
+++ src/LTC2942.cpp
@@ -147,7 +147,7 @@ void LTC2942::setPrescalerM(uint8_t m) {
 void LTC2942::setBatteryCapacity(uint16_t mAh) {
 	_batteryCapacity = mAh;		
     unsigned int k, a;
-    uin8_t m = 7;
+    uint8_t m = 7;
     for(k = 128; k > 1; k = k / 2) {
         a = 278524 * k / _rSense / 128;
         if (a < (2 * mAh)) {
